555f3f95b9c2631d9e69c3a9a9a8a6e40a9ec4c6
srshoruv <srshoruv2002@gmail.com>
2024-07-22 12:32:10 +0600

    first window

---
+++ numberGame.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cout << "All the available functions: \n 1. Calculator \n 2. Even or Odd \n 3. Factorial \n 4. Check Prime Number \n 5. N Prime Numbers \n 6. N Fibonacci \n 7. Multiplication Table \n 8. Check Armstrong Number \n Choose an option: " ;
+    cin >> input;
+    return 0;
+}
